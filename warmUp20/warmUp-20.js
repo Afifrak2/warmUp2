5e8655dbfae3562e9e95e3c847111131fb3660d9
Afif Rakrouki <afifrak2@gmail.com>
2020-02-28 09:38:15 +0100

    warmup20

--- warmUp20/warmUp-20.js
+++ warmUp20/warmUp-20.js
@@ -4,3 +4,12 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+function reverseStr(string) {
+    var array=string.split(' ');
+    var str='';
+    for (var i=array.length-1; i>=0;i--) {
+        str=str+' '+array[i];
+    }
+    return str.slice(1);
+}
